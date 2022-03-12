dbda021e544d5c390ad5860b6e2f91db8168b79d
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-03-11 16:31:11 -0800

    add footer and styling updates

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -10,7 +10,6 @@ namespace HairSalon.Models
     }
 
     public int StylistId { get; set; }
-    // [Required(ErrorMessage = "Stylist Name can't be blank!")]
     public string StylistName { get; set; }
     public virtual ICollection<Client> Clients { get; set; }
   }
